d9724a6a0ab5471d2527518bd369a8d93929549e
Samantha Geitz <samanthamichele7@gmail.com>
2013-04-15 19:58:38 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Anarchum::Application.config.secret_token = 'ea121d0b4f10d16364081d36d953a400600c7150192afae222817347a3d2ab2fc7708aa615006db8314c20f6d089d2713e4613b2065cabf233ed10cc0ab9776e'
