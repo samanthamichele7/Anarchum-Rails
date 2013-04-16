c2f44693882613c0e6b076b7fe67463d918da9e7
Samantha Geitz <samanthamichele7@gmail.com>
2013-04-16 01:37:10 -0500

    Added more content to homepage

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Anarchum::Application.routes.draw do
     root :to => 'home#index'
   end
   root :to => "home#index"
+
+  as :user do
+    get '/register', to: 'devise/registrations#new', as: :register
+    get '/login',    to: 'devise/sessions#new',       as: :login
+    get '/logout',    to: 'devise/sessions#destroy',       as: :logout
+  end
+  
   devise_for :users
   resources :users
 end
